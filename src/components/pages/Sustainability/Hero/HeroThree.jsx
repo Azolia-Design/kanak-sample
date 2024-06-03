06ba49cb9c1fc379502a28962e39dbf9562b6f2c
nqnduy <nqn.duy@gmail.com>
2024-06-03 18:20:21 +0700

    Update 3D

--- src/components/pages/Sustainability/Hero/HeroThree.jsx
+++ src/components/pages/Sustainability/Hero/HeroThree.jsx
@@ -44,14 +44,27 @@ function Content(props) {
         scroll(({ y }) => {
             if (!modelWrapRef.current) return;
             if (y.progress >= 0 && y.progress < 1) {
-                modelWrapRef.current.position.set(animThreeVal(0 / scaleOffset, .3 / scaleOffset, y.progress), animThreeVal(-.6 / scaleOffset, -.4 / scaleOffset, y.progress), animThreeVal(0 / scaleOffset, -.1 / scaleOffset, y.progress))
-                modelWrapRef.current.rotation.set(animThreeValRot(0, .4, y.progress), animThreeValRot(0, .02, y.progress), animThreeValRot(0, -.35, y.progress))
+                modelWrapRef.current.position.set(0, animThreeVal(-.6 / scaleOffset, -.4 / scaleOffset, y.progress), 0)
+                modelWrapRef.current.rotation.set(animThreeValRot(0, .1, y.progress), 0, 0)
 
                 contactShadow.current.position.y = animThreeVal(-1.1 / scaleOffset, -1.2 / scaleOffset, y.progress)
             }
         }, {
+                target: document.querySelector('.sustainable-hero') ,
+                offset: ["start start", "end 50%"]
+        })
+
+        scroll(({ y }) => {
+            if (!modelWrapRef.current) return;
+            if (y.progress > 0 && y.progress <= 1) {
+                modelWrapRef.current.position.set(animThreeVal(0 / scaleOffset, .3 / scaleOffset, y.progress), animThreeVal(-.4 / scaleOffset, -.4 / scaleOffset, y.progress), animThreeVal(0 / scaleOffset, -.1 / scaleOffset, y.progress))
+                modelWrapRef.current.rotation.set(animThreeValRot(.1, .4, y.progress), animThreeValRot(0, .02, y.progress), animThreeValRot(0, -.35, y.progress))
+
+                contactShadow.current.position.y = animThreeVal(-1.2 / scaleOffset, -1.25 / scaleOffset, y.progress)
+            }
+        }, {
                 target: document.querySelector('.sustainable-intro') ,
-                offset: ["start end", "end 50%"]
+                offset: ["start 50%", "end 50%"]
         })
         scroll(({ y }) => {
             if (!modelWrapRef.current) return;
@@ -60,7 +73,7 @@ function Content(props) {
                 modelWrapRef.current.rotation.set(animThreeValRot(0.4, 2.2, y.progress), animThreeValRot(.02, -1, y.progress), animThreeValRot(-.35, -.2, y.progress))
                 modelWrapRef.current.scale.set(animThreeVal(7 / scaleOffset, 7.4 / scaleOffset, y.progress), animThreeVal(7 / scaleOffset, 7.4 / scaleOffset, y.progress), animThreeVal(7 / scaleOffset, 7.4 / scaleOffset, y.progress))
 
-                contactShadow.current.position.y = animThreeVal(-1.2 / scaleOffset, -1.35 / scaleOffset, y.progress)
+                contactShadow.current.position.y = animThreeVal(-1.25 / scaleOffset, -1.35 / scaleOffset, y.progress)
             }
         }, {
                 target: document.querySelector('.sustainable-practice-wrap') ,
